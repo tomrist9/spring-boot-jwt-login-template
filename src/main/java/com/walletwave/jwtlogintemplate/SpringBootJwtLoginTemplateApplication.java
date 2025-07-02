239820f38d2501646ba2cc9401f698d6f67adcbb
Tomris Teymurlu <tomristt9@gmail.com>
2025-07-02 12:22:59 +0400

    first commit

---
+++ src/main/java/com/walletwave/jwtlogintemplate/SpringBootJwtLoginTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.walletwave.jwtlogintemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtLoginTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtLoginTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walletwave/jwtlogintemplate/SpringBootJwtLoginTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletwave.jwtlogintemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtLoginTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
